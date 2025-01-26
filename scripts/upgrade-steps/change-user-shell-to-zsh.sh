7e09197ffd0fa6e17a86267cea32768a2c5a9deb
Niapoll <Kolya21082001@gmail.com>
2025-01-26 22:49:13 +0300

    Fix issue with identifying user shell

--- scripts/upgrade-steps/change-user-shell-to-zsh.sh
+++ scripts/upgrade-steps/change-user-shell-to-zsh.sh
@@ -2,9 +2,10 @@
 set -eo pipefail
 
 USER_TO_CHANGE="${1:-root}"
+OLD_SHELL="$(getent passwd "$USER_TO_CHANGE" | awk -F: '{print $NF}')"
 NEW_SHELL='/bin/zsh'
 
-if [ "$SHELL" != "$NEW_SHELL" ]; then
+if [ "$NEW_SHELL" != "$OLD_SHELL" ]; then
     echo "[*] Set \"$NEW_SHELL\" shell as default for \"$USER_TO_CHANGE\" user"
     sudo chsh -s "$NEW_SHELL" "$USER_TO_CHANGE"
 fi
